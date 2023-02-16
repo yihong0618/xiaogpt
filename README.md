0463a3ff153a1168ccf5b0a1330d101cf4ffdf2b
yihong0618 <zouzou0208@gmail.com>
2023-02-16 21:39:19 +0800

    fix

--- README.md
+++ README.md
@@ -7,25 +7,22 @@ play chatgpt with xiaomi ai speaker
 2. 小爱音响
 3. 能正常联网的环境或 proxy
 4. python3.8+
-4. 抓包环境
 
 ## 使用
 
-1. 在`小爱音响` APP 点对话记录并抓包找到 https://userprofile.mina.mi.com/device_profile/v2/conversation 这个 api 的 cookie
-2. pip install -r requirements.txt
-3. 参考 [MiService](https://github.com/Yonsm/MiService) 项目 README 并在本地 terminal 跑 `micli list` 拿到你音响的 DID 成功 **别忘了设置 export MI_DID=xxx** 这个 MI_DID 用 
-4. 参考 [revChatGPT](https://github.com/acheong08/ChatGPT) 项目 README 配置 chatGPT 的 config
-5. run `python xiaogpt.py ${cookie} --hardware ${your_hardware}` ${cookie} 为你抓包的 cookie hardware 你看小爱屁股上有型号，输入进来
-6. 跑起来之后就可以问小爱同学问题了，“帮我回答开头的问题” 会发送一份给 ChatGPT 然后小爱同学用 tts 回答
+1. pip install -r requirements.txt
+2. 参考 [MiService](https://github.com/Yonsm/MiService) 项目 README 并在本地 terminal 跑 `micli list` 拿到你音响的 DID 成功 **别忘了设置 export MI_DID=xxx** 这个 MI_DID 用 
+3. 参考 [revChatGPT](https://github.com/acheong08/ChatGPT) 项目 README 配置 chatGPT 的 config
+4. run `python xiaogpt.py --hardware ${your_hardware}` hardware 你看小爱屁股上有型号，输入进来
+5. 跑起来之后就可以问小爱同学问题了，“帮我回答开头的问题” 会发送一份给 ChatGPT 然后小爱同学用 tts 回答
 
 ## QA
 
 1. 用破解么？不用
-2. 为啥必须抓包？我之后再看看，争取不用抓包
-3. 连不上 revChatGPT？国情，你得设置 proxy 并且该地区可用的 proxy
-4. 你做这玩意也没用啊？确实。。。但是挺好玩的，有用对你来说没用，对我们来说不一定呀
-5. 想把它变得更好？PR Issue always welcome.
-6. 还有问题？提 Issuse 哈哈
+2. 连不上 revChatGPT？国情，你得设置 proxy 并且该地区可用的 proxy
+3. 你做这玩意也没用啊？确实。。。但是挺好玩的，有用对你来说没用，对我们来说不一定呀
+4. 想把它变得更好？PR Issue always welcome.
+5. 还有问题？提 Issuse 哈哈
 
 ## 赞赏
 
--- xiaogpt.py
+++ xiaogpt.py
@@ -3,17 +3,21 @@ import json
 import subprocess
 import time
 from http.cookies import SimpleCookie
+from pathlib import Path
 
 import requests
 from requests.utils import cookiejar_from_dict
 from revChatGPT.V1 import Chatbot, configure
 from rich import print
 
-s = requests.session()
-
 LATEST_ASK_API = "https://userprofile.mina.mi.com/device_profile/v2/conversation?source=dialogu&hardware={hardware}&timestamp={timestamp}&limit=2"
+COOKIE_TEMPLATE = "deviceId={device_id}; serviceToken={service_token}; userId={user_id}"
 
-COOKIE_STRING = ""
+HARDWARE_COMMAND_DICT = {
+    "LX06": "5-1",
+    "L05B": "5-3",
+    # add more here
+}
 
 
 ### HELP FUNCTION ###
@@ -28,71 +32,132 @@ def parse_cookie_string(cookie_string):
     return cookiejar
 
 
-def get_latest_ask_from_xiaoai(hardware="LX06"):
-    s.cookies = parse_cookie_string(COOKIE_STRING)
-    r = s.get(
-        LATEST_ASK_API.format(hardware=hardware, timestamp=str(int(time.time() * 1000)))
-    )
-    return r.json()
+class MiGPT:
+    def __init__(self, hardware):
+        self.mi_token_home = Path.home() / ".mi.token"
+        self.hardware = hardware
+        self.cookie_string = ""
+        self.last_timestamp = 0  # timestamp last call mi speaker
+        self.s = requests.session()
+        self.chatbot = None  # a little slow to init we move it after xiaomi init
+        self.user_id = ""
+        self.device_id = ""
+        self.service_token = ""
+        self.tts_command = HARDWARE_COMMAND_DICT.get(hardware, "5-1")
 
+    def _init_all_data(self):
+        # Step 1 make sure we init the ai api and servive token
+        try:
+            # micli mina to make sure the init
+            mi_hardware_data = json.loads(subprocess.check_output(["micli", "mina"]))
+        except Exception as e:
+            print(str(e))
+            raise Exception(
+                "Something is wrong with get data, please visit https://github.com/Yonsm/MiService"
+            )
 
-def get_last_timestamp_and_record(data):
-    if d := data.get("data"):
-        records = json.loads(d).get("records")
-        last_record = records[0]
-        timestamp = last_record.get("time")
-        return timestamp, last_record
-    else:
-        return 0, None
+        # Step 2 get user_id and ai service_token
+        with open(self.mi_token_home) as f:
+            user_data = json.loads(f.read())
+        self.user_id = user_data.get("userId")
+        self.service_token = user_data.get("micoapi")[1]
+        self._init_data_hardware(mi_hardware_data)
+        # Step 3 init get get message api's cookie
+        self._init_cookie()
+        # Step 4 init first data and chatbot
+        self._init_first_data_and_chatbot()
 
-
-def main(hardware):
-    data = get_latest_ask_from_xiaoai(hardware=hardware)
-    timestamp, last_record = get_last_timestamp_and_record(data)
-    c = Chatbot(configure())
-    while 1:
-        print(f"Now listening xiaoai new message timestamp: {timestamp}")
-        r = get_latest_ask_from_xiaoai(hardware=hardware)
-        time.sleep(5)
-        new_timestamp, last_record = get_last_timestamp_and_record(r)
-        if new_timestamp > timestamp:
-            timestamp = new_timestamp
-            query = last_record.get("query", "")
-            if query.find("帮我回答") != -1:
-                # drop 帮我回答
-                query = query[4:] + "，请用100字以内回答"
-                print(query)
-                print("Running chatgpt ask maybe a little slow we do not pay")
-                # waiting for xiaoai speaker done
-                time.sleep(5)
-                subprocess.check_output(["micli", "5-1", "正在问GPT我们不是会员还用的API有点慢"])
-                data = list(c.ask(query))[-1]
-                if message := data.get("message", ""):
-                    # xiaoai tts did not support space
-                    message = message.replace(" ", ",")
-                    message = "以下是GPT的回答，" + message
-                    print(message)
-                    try:
-                        print(
-                            "以下是小爱的回答",
-                            last_record.get("answers")[0].get("tts", {}).get("text"),
-                        )
-                    except:
-                        print("小爱没回")
-                    # 5-1 for xiaoai pro tts
-                    try:
-                        subprocess.check_output(["micli", "5-1", message])
-                        time.sleep(1)
-                    except Exception as e:
-                        print("Something is wrong: ", str(e))
-
+    def _init_data_hardware(self, hardware_data):
+        for h in hardware_data:
+            if h.get("hardware", "") == self.hardware:
+                self.device_id = h.get("deviceID")
+                break
         else:
-            print("No new xiao ai record")
+            raise Exception(f"we have no hardware: {self.hardware} please check")
+
+    def _init_cookie(self):
+        self.cookie_string = COOKIE_TEMPLATE.format(
+            device_id=self.device_id,
+            service_token=self.service_token,
+            user_id=self.user_id,
+        )
+        self.s.cookies = parse_cookie_string(self.cookie_string)
+
+    def _init_first_data_and_chatbot(self):
+        data = self.get_latest_ask_from_xiaoai()
+        self.last_timestamp, self.last_record = self.get_last_timestamp_and_record(data)
+        self.chatbot = Chatbot(configure())
+
+    def get_latest_ask_from_xiaoai(self):
+        r = self.s.get(
+            LATEST_ASK_API.format(
+                hardware=self.hardware, timestamp=str(int(time.time() * 1000))
+            )
+        )
+        return r.json()
+
+    def get_last_timestamp_and_record(self, data):
+        if d := data.get("data"):
+            records = json.loads(d).get("records")
+            last_record = records[0]
+            timestamp = last_record.get("time")
+            return timestamp, last_record
+        else:
+            return 0, None
+
+    def run_forever(self):
+        self._init_all_data()
+        while 1:
+            print(f"Now listening xiaoai new message timestamp: {self.last_timestamp}")
+            try:
+                r = self.get_latest_ask_from_xiaoai()
+            except Exception:
+                # we try to init all again
+                self._init_all_data()
+                r = self.get_latest_ask_from_xiaoai()
+
+            time.sleep(5)
+            new_timestamp, last_record = self.get_last_timestamp_and_record(r)
+            if new_timestamp > self.last_timestamp:
+                self.last_timestamp = new_timestamp
+                query = last_record.get("query", "")
+                if query.find("帮我回答") != -1:
+                    # drop 帮我回答
+                    query = query[4:] + "，请用100字以内回答"
+                    print(query)
+                    print("Running chatgpt ask maybe a little slow we do not pay")
+                    # waiting for xiaoai speaker done
+                    time.sleep(8)
+                    subprocess.run(["micli", self.tts_command, "正在问GPT我们不是会员还用的API有点慢"])
+                    data = list(self.chatbot.ask(query))[-1]
+                    if message := data.get("message", ""):
+                        # xiaoai tts did not support space
+                        message = message.replace(" ", ",")
+                        message = "以下是GPT的回答:" + message
+                        print(message)
+                        try:
+                            print(
+                                "以下是小爱的回答: ",
+                                last_record.get("answers")[0]
+                                .get("tts", {})
+                                .get("text"),
+                            )
+                        except:
+                            print("小爱没回")
+                        # 5-1 for xiaoai pro tts
+                        # TODO more data to chunk
+                        try:
+                            subprocess.run(["micli", self.tts_command, message])
+                            time.sleep(1)
+                        except Exception as e:
+                            print("Something is wrong: ", str(e))
+
+            else:
+                print("No new xiao ai record")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("cookie", help="xiao ai cookie")
     parser.add_argument(
         "--hardware",
         dest="hardware",
@@ -101,5 +166,6 @@ if __name__ == "__main__":
         help="小爱 hardware",
     )
     options = parser.parse_args()
-    COOKIE_STRING = options.cookie
-    main(options.hardware)
+    miboy = MiGPT(options.hardware)
+    miboy._init_all_data()
+    miboy.run_forever()
