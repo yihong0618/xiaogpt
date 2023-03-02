b06f934d91193d3c16c91555f474af3e42dc1d27
Tiancheng Jiao <43341500+jtc1246@users.noreply.github.com>
2023-03-02 23:42:31 +0800

    Update xiaogpt.py

--- xiaogpt.py
+++ xiaogpt.py
@@ -74,47 +74,31 @@ class GPT3Bot:
 
 class ChatGPTBot:
     def __init__(self, session):
-        self.history=[] # added by jtc
-        self.time=round(time.time()*1000) # added by jtc
-        pass
+        self.history = []
 
     async def ask(self, query):
         openai.api_key = OPENAI_API_KEY
-        # added by jtc: start
-        ms=[]
-        if(len(self.history)>=5):
-            l=len(self.history)
-            ms=[
-                {'role': 'user', 'content': self.history[-5][0]},
-                {'role': 'assistant', 'content': self.history[-5][1]},
-                {'role': 'user', 'content': self.history[-4][0]},
-                {'role': 'assistant', 'content': self.history[-4][1]},
-                {'role': 'user', 'content': self.history[-3][0]},
-                {'role': 'assistant', 'content': self.history[-3][1]},
-                {'role': 'user', 'content': self.history[-2][0]},
-                {'role': 'assistant', 'content': self.history[-2][1]},
-                {'role': 'user', 'content': self.history[-1][0]},
-                {'role': 'assistant', 'content': self.history[-1][1]},
-                {'role': 'user', 'content': f"{query}"}
+        ms = []
+        if len(self.history) >= 5:
+            ms = [
+                {"role": "user", "content": self.history[-5][0]},
+                {"role": "assistant", "content": self.history[-5][1]},
+                {"role": "user", "content": self.history[-4][0]},
+                {"role": "assistant", "content": self.history[-4][1]},
+                {"role": "user", "content": self.history[-3][0]},
+                {"role": "assistant", "content": self.history[-3][1]},
+                {"role": "user", "content": self.history[-2][0]},
+                {"role": "assistant", "content": self.history[-2][1]},
+                {"role": "user", "content": self.history[-1][0]},
+                {"role": "assistant", "content": self.history[-1][1]},
+                {"role": "user", "content": f"{query}"},
             ]
         else:
             for h in self.history:
-                ms.append({'role': 'user', 'content': h[0]})
-                ms.append({'role': 'assistant', 'content': h[1]})
-            ms.append({'role': 'user', 'content': f"{query}"})
-        # added by jtc: end
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            # commented by jtc
-            # messages=[
-            #     {
-            #         "role": "user",
-            #         "content": f"{query}",
-            #     }
-            # ],
-
-            messages=ms # added by jtc
-        )
+                ms.append({"role": "user", "content": h[0]})
+                ms.append({"role": "assistant", "content": h[1]})
+            ms.append({"role": "user", "content": f"{query}"})
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=ms)
         message = (
             completion["choices"][0]
             .get("message")
@@ -122,12 +106,7 @@ class ChatGPTBot:
             .encode("utf8")
             .decode()
         )
-        # added by jtc: start
-        self.history.append([f"{query}",message])
-        f=open(f'./log-{self.time}.txt','w')
-        f.write(json.dumps(self.history,ensure_ascii=False))
-        f.close()
-        # added by jtc: end
+        self.history.append([f"{query}", message])
         return message
 
 
