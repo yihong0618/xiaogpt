606215ba62c0e1953470016c5a697bd149ed7b27
Frost Ming <me@frostming.com>
2024-01-08 15:23:47 +0800

    fix: eliminate the deprecation warings from langchain (#423)
    Fixes #422
    
    Signed-off-by: Frost Ming <me@frostming.com>

--- xiaogpt/langchain/chain.py
+++ xiaogpt/langchain/chain.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.chains import LLMMathChain
-from langchain.chat_models import ChatOpenAI
 from langchain.schema.memory import BaseMemory
-from langchain.utilities import SerpAPIWrapper
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.utilities import SerpAPIWrapper
 
 
 async def agent_search(
@@ -23,8 +23,16 @@ async def agent_search(
 
     # Tool list: search, mathematical calculations
     tools = [
-        Tool(name="Search", func=search.run, description="如果你不知道或不确定答案，可以使用这个搜索引擎检索答案"),
-        Tool(name="Calculator", func=llm_math_chain.run, description="在需要回答数学问题时非常有用"),
+        Tool(
+            name="Search",
+            func=search.run,
+            description="如果你不知道或不确定答案，可以使用这个搜索引擎检索答案",
+        ),
+        Tool(
+            name="Calculator",
+            func=llm_math_chain.run,
+            description="在需要回答数学问题时非常有用",
+        ),
     ]
 
     agent = initialize_agent(
